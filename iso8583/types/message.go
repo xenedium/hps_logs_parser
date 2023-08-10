6f95584bc4e8c4f4eae96199a095943b44f96cd4
xenedium <ahmed@abderraziq.com>
2023-08-10 11:07:24 +0100

    More helper functions

--- iso8583/types/message.go
+++ iso8583/types/message.go
@@ -28,4 +28,26 @@ func (m *message) GetMTIOriginName() string {
 	return m.MTI.getMTIOriginName()
 }
 
+func (m *message) IsRequest() bool {
+	return m.MTI.isRequest()
+}
+
+func (m *message) IsResponse() bool {
+	return m.MTI.isResponse()
+}
+
+func (m *message) GetField(field int) (Field, bool) {
+	f, ok := m.Fields[field]
+	return f, ok
+}
+
+func (m *message) SetField(field int, value string) *message {
+	m.Fields[field] = Field{Length: len(value), Value: value}
+	return m
+}
+
+func (m *message) GetRaw() []byte {
+	return m.Raw
+}
+
 type Message = message
