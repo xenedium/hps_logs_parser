fcbe90b62e71c44149270ab65f28582db44d0005
xenedium <ahmed@abderraziq.com>
2023-08-23 10:09:51 +0100

    Renamed parsers file

---
+++ iso8583/parser/dump_parsers.go
@@ -0,0 +1,21 @@
+package parser
+
+import (
+	"time"
+)
+
+func (p *parser) parseDumpPostilion(dumpPostilion string, fileName string) {
+
+}
+
+func (p *parser) parseDumpXml(dumpXml string, fileName string) {
+	time.Sleep(1 * time.Second)
+}
+
+func (p *parser) parseDumpIso(dumpIso string, fileName string) {
+	time.Sleep(1 * time.Second)
+}
+
+func (p *parser) parseDumpTlvBuffer(dumpTlvBuffer string, fileName string) {
+	time.Sleep(1 * time.Second)
+}
