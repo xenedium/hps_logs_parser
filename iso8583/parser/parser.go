8b853fc0e09111478188e8b1c612963d24567741
xenedium <ahmed@abderraziq.com>
2023-08-25 12:24:51 +0100

    Changed Parser default instantiation

--- iso8583/parser/parser.go
+++ iso8583/parser/parser.go
@@ -1,9 +1,11 @@
 package parser
 
 import (
+	"fmt"
 	"github.com/xenedium/hps_logs_parser/iso8583/scanner"
 	"github.com/xenedium/hps_logs_parser/iso8583/types"
 	"os"
+	"path"
 	"sync"
 )
 
@@ -17,7 +19,38 @@ type parser struct {
 	ParsedDumpTlvBuffers []*types.Message
 }
 
+func NewParser(logDir string) *Parser {
+	files, err := os.ReadDir(logDir)
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+
+	var filesToParse []*os.File
+
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+		f, err := os.Open(path.Join(logDir, file.Name()))
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+		filesToParse = append(filesToParse, f)
+	}
+
+	return &parser{
+		Files: filesToParse,
+	}
+}
+
 func (p *parser) Parse() []*types.Message {
+	defer func() {
+		for _, file := range p.Files {
+			_ = file.Close()
+		}
+	}()
 	scanWaitGroup := sync.WaitGroup{}
 	for _, file := range p.Files {
 		scanWaitGroup.Add(1)
