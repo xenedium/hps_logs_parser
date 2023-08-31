114d0e73407afee0c86144e4f8c1031156d52497
xenedium <ahmed@abderraziq.com>
2023-08-31 14:01:35 +0100

    Impl routing

--- iso8583frontend/src/App.tsx
+++ iso8583frontend/src/App.tsx
@@ -3,139 +3,22 @@ import {AppShell} from '@mantine/core';
 import {Sidenav} from './components/Sidenav.tsx';
 import {useEffect, useState} from 'react';
 import NewParse from './pages/NewParse.tsx';
+import ParseResult from './pages/ParseResult.tsx';
+import {IParseResult} from './types.ts';
 
-const mockData = [
+const mockData: Omit<IParseResult, 'messages'>[] = [
     {
         id: 1,
-        name: 'parse-request-1',
+        name: 'parse-result-1',
         date: new Date(),
-    },
-    {
-        id: 2,
-        name: 'parse-request-2',
-        date: new Date(),
-    },
-    {
-        id: 3,
-        name: 'parse-request-3',
-        date: new Date(),
-    },
-    {
-        id: 4,
-        name: 'parse-request-4',
-        date: new Date(),
-    },
-    {
-        id: 5,
-        name: 'parse-request-5',
-        date: new Date(),
-    },
-    {
-        id: 6,
-        name: 'parse-request-6',
-        date: new Date(),
-    },
-    {
-        id: 7,
-        name: 'parse-request-7',
-        date: new Date(),
-    },
-    {
-        id: 8,
-        name: 'parse-request-8',
-        date: new Date(),
-    },
-    {
-        id: 9,
-        name: 'parse-request-9',
-        date: new Date(),
-    },
-    {
-        id: 10,
-        name: 'parse-request-10',
-        date: new Date(),
-    },
-    {
-        id: 11,
-        name: 'parse-request-11',
-        date: new Date(),
-    },
-    {
-        id: 12,
-        name: 'parse-request-12',
-        date: new Date(),
-    },
-    {
-        id: 13,
-        name: 'parse-request-13',
-        date: new Date(),
-    },
-    {
-        id: 14,
-        name: 'parse-request-14',
-        date: new Date(),
-    },
-    {
-        id: 15,
-        name: 'parse-request-15',
-        date: new Date(),
-    },
-    {
-        id: 16,
-        name: 'parse-request-16',
-        date: new Date(),
-    },
-    {
-        id: 17,
-        name: 'parse-request-17',
-        date: new Date(),
-    },
-    {
-        id: 18,
-        name: 'parse-request-18',
-        date: new Date(),
-    },
-    {
-        id: 19,
-        name: 'parse-request-19',
-        date: new Date(),
-    },
-    {
-        id: 20,
-        name: 'parse-request-20',
-        date: new Date(),
-    },
-    {
-        id: 21,
-        name: 'parse-request-21',
-        date: new Date(),
-    },
-    {
-        id: 22,
-        name: 'parse-request-22',
-        date: new Date(),
-    },
-    {
-        id: 23,
-        name: 'parse-request-23',
-        date: new Date(),
-    },
-    {
-        id: 24,
-        name: 'parse-request-24',
-        date: new Date(),
-    },
-    {
-        id: 25,
-        name: 'parse-request-25',
-        date: new Date(),
+        type: 'ssh',
+        status: 'done',
     }
 ]
 
-// TODO: implement routing
 function App() {
-    const [parses, setParses] = useState<typeof mockData>([]);
-    const [selection, setSelection] = useState<number>(1);
+    const [parses, setParses] = useState<Omit<IParseResult, 'messages'>[]>([]);
+    const [selection, setSelection] = useState<number | 'new-parse'>('new-parse');
 
     useEffect(() => {
         // TODO: fetch parses from backend
@@ -145,9 +28,19 @@ function App() {
     return (
         <AppShell
             padding="md"
-            navbar={<Sidenav parses={parses} selection={selection} setSelection={(id: number) => setSelection(id)}/>}
+            navbar={
+                <Sidenav
+                    parses={parses}
+                    selection={selection}
+                    setSelection={(newSelection: number | 'new-parse') => setSelection(newSelection)}
+                />
+            }
         >
-            <NewParse />
+            {
+                selection === 'new-parse' ?
+                    <NewParse/> :
+                    <ParseResult id={selection} />
+            }
         </AppShell>
     )
 }
